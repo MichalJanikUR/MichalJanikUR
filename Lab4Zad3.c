6e8657ccbdbaf9b245d75fdb069b94bd30e7053b
student <student@ur.edu.pl>
2025-01-18 16:22:51 +0100

    Updated Lab4Zad3.c

--- Lab4Zad3.c
+++ Lab4Zad3.c
@@ -7,17 +7,18 @@ int main()
 	printf("Podaj tekst: ");
 	fgets(tekst, sizeof(tekst), stdin);
 
-	int licznik_sm = 0, licznik_sp=0;
+	int licz_samo = 0, licz_spol = 0, licz_spac = 0;
 
 	for (int i = 0; tekst[i] != '\0'; i++)
 	{
 		if (tekst[i] == 'a' || tekst[i] == 'e' || tekst[i] == 'i' || tekst[i] == 'o' || tekst[i] == 'u' || tekst[i] == 'y' || tekst[i] == '¹' || tekst[i] == 'ê' || tekst[i] == 'ó'
-			|| tekst[i] == 'A' || tekst[i] == 'E' || tekst[i] == 'I' || tekst[i] == 'O' || tekst[i] == 'U' || tekst[i] == 'Y' || tekst[i] == '¥' || tekst[i] == 'Ê' || tekst[i] == 'Ó') licznik_sm++;
-		else if (tekst[i] != ' ') licznik_sp++;
+			|| tekst[i] == 'A' || tekst[i] == 'E' || tekst[i] == 'I' || tekst[i] == 'O' || tekst[i] == 'U' || tekst[i] == 'Y' || tekst[i] == '¥' || tekst[i] == 'Ê' || tekst[i] == 'Ó') licz_samo++;
+		else if (tekst[i] == ' ') licz_spac++;
+		else licz_spol++;
 	}
 
-	printf("Liczba samoglosek: %d", licznik_sm);
-	printf("\nLiczba spolglosek: %d\n", licznik_sp);
+	printf("Liczba samoglosek: %d", licz_samo);
+	printf("\nLiczba spolglosek: %d", licz_spol-1);
 
 	return 0;
 }
